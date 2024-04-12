b082966c03e13b3b2cbe5986f4a59c8d53537229
John Mellet <JohnMellet@outlook.com>
2024-04-12 01:43:09 +0100

    Update to close button
    -Added function to check if there is text or checked radio inputs and if there is open modal, if not exit to home

--- scriptCattleInput.js
+++ scriptCattleInput.js
@@ -1,6 +1,9 @@
 const openEntryForm = document.getElementById("add-entry-btn");
 const cattleHome = document.querySelector(".cattle-home");
 const cattleEntryForm = document.querySelector(".cattle-entry-form");
+const dialogModal = document.querySelector(".confirm-close-dialog");
+const closeEntryBtn = document.getElementById("close-form-btn");
+const nameInput = document.getElementById("cattle-name");
 
 function toggleHidden() {
   cattleHome.classList.toggle("hidden");
@@ -9,4 +12,20 @@ function toggleHidden() {
 
 openEntryForm.addEventListener("click", () => {
   toggleHidden();
+});
+
+function checkCattleInput() {
+  const checkRadioButtons = document.querySelectorAll(
+    'input[type="radio"]:checked'
+  );
+
+  if (checkRadioButtons.length > 0 || nameInput.value.trim() !== "") {
+    dialogModal.showModal();
+  } else {
+    toggleHidden();
+  }
+}
+
+closeEntryBtn.addEventListener("click", () => {
+  checkCattleInput();
 });
